8a4d585ec1c812aae30e6d122b478e041f5285f5
Hayden <haydenlauritzen@users.noreply.github.com>
2022-06-10 19:08:17 -0700

    Render first piece

--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +14,101 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Engine;
 
 namespace UI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+
+    public partial class MainWindow : INotifyPropertyChanged
     {
+
+        private Board _chessboard = new Board();
+        public Board ChessBoard
+        {
+            get { return _chessboard; }
+            set
+            {
+                _chessboard = value;
+                // RaisePropetyChanged("M");
+            }
+        }
+
         public MainWindow()
         {
+            _chessboard.setBoard(Board.debug);
+            DataContext = this;
             InitializeComponent();
+        }
+
+        private void Image_Loaded(object sender, RoutedEventArgs e)
+        {
+            Image? img = sender as Image;
+            if (img == null) return;
+            string? tile = img.Tag.ToString();
+            if (tile == null) return;
+            int piece = _chessboard.getTile(tile);
+            switch (piece)
+            {
+                case Piece.Empty:
+                    {
+                        break;
+                    }
+                case Piece.White + Piece.Pawn:
+                    {
+                        break;
+                    }
+                case Piece.White + Piece.Knight:
+                    {
+                        break;
+                    }
+                case Piece.White + Piece.Bishop:
+                    {
+                        break;
+                    }
+                case Piece.White + Piece.Rook:
+                    {
+                        break;
+                    }
+                case Piece.White + Piece.Queen:
+                    {
+                        break;
+                    }
+                case Piece.White + Piece.King:
+                    {   
+                        img.Source = new BitmapImage(new Uri(@"pack://application:,,,/Assets/King_White.png", UriKind.Absolute));
+                        break;
+                    }
+                case Piece.Black + Piece.Pawn:
+                    {
+                        break;
+                    }
+                case Piece.Black + Piece.Knight:
+                    {
+                        break;
+                    }
+                case Piece.Black + Piece.Bishop:
+                    {
+                        break;
+                    }
+                case Piece.Black + Piece.Rook:
+                    {
+                        break;
+                    }
+                case Piece.Black + Piece.Queen:
+                    {
+                        break;
+                    }
+                case Piece.Black + Piece.King:
+                    {
+                        break;
+                    }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
